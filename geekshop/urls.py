72dfe2c3abc4d10e755f652f19b672dd1a0c9f05
egor <ickzn@ya.ru>
2021-01-28 11:18:31 +0300

    dz3

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -17,7 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from mainapp.views import index , products , test_context
+from mainapp.views import index , products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +29,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # urlpatterns+=static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
